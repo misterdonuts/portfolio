1cf0d798f9ecbf567f6049f42dfacc49fa06e75f
adshidtadka <adshidtadka>
2019-03-21 21:51:28 +0900

    WIP create html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-jQuery( '.drawer-item a' ).on( 'click', function() {
-	jQuery( '#drawer-check' ).prop( 'checked', false );
+$(function() {
+	$(window).scroll(function() {
+		$(".fadein").each(function() {
+			var targetElement = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > targetElement - windowHeight + 200) {
+				$(this).css("opacity", "1");
+				$(this).css("transform", "translateY(0)");
+			}
+		});
+	});
 });
